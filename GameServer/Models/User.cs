b3262d4b948f7153028a01a5ae4e179861db9d23
jeongju <ryan20115@naver.com>
2025-01-06 14:30:48 +0900

    250106 서버 초기 세팅

---
+++ GameServer/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServer.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public required string NickName { get; set; }
+        public required string SessionId { get; set; }
+    }
+}
