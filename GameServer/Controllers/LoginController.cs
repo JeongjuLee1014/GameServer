743cad74e439ff5b0579b196b53a046532789f1b
KimYeeun <nabinabi1110@gmail.com>
2025-01-07 01:21:57 +0900

    googl login

--- GameServer/Controllers/LoginController.cs
+++ GameServer/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using dotenv.net;
 
 namespace kakaoTemp.Controllers
 {
@@ -6,6 +7,20 @@ namespace kakaoTemp.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        private readonly string _googleClientId;
+
+        public LoginController()
+        {
+
+            // 환경 변수에서 Client ID 읽기
+            _googleClientId = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID");
+
+            if (string.IsNullOrEmpty(_googleClientId))
+            {
+                throw new Exception("Google OAuth 환경 변수가 설정되지 않았습니다.");
+            }
+        }
+
         [HttpGet("kakao")]
         public IActionResult RedirectToKakaoLogin([FromQuery] string session_id)
         {
@@ -23,18 +38,37 @@ namespace kakaoTemp.Controllers
             return Redirect(redirectUrl);
         }
 
+        [HttpGet("naver")]
+        public IActionResult RedirectToNaverLogin([FromQuery] string session_id)
+        {
+            HttpContext.Session.SetString("SessionId", session_id);
+
+            const string client_id = "pKaHA3PgKs1zqls08bAy";
+            const string redirect_uri = "https://localhost:7032/oauth/naver";
+            const string response_type = "code";
+            const string state = "1234";
+
+            const string redirectUrl = "https://nid.naver.com/oauth2.0/authorize"
+                + "?response_type=" + response_type
+                + "&client_id=" + client_id
+                + "&redirect_uri=" + redirect_uri
+                + "&state=" + state;
+
+            return Redirect(redirectUrl);
+        }
+
         [HttpGet("google")]
         public IActionResult RedirectToGoogleLogin([FromQuery] string session_id)
         {
             HttpContext.Session.SetString("SessionId", session_id);
 
-            const string client_id = "631548192030-u31u1vn5o4343nh85ksuvrge72l6aq9s.apps.googleusercontent.com";
+
             const string redirect_uri = "https://localhost:7032/oauth/google";
             const string response_type = "code";
             const string scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
 
             string redirectUrl = "https://accounts.google.com/o/oauth2/v2/auth" +
-            "?client_id=" + client_id +
+            "?client_id=" + _googleClientId +
             "&redirect_uri=" + redirect_uri +
             "&response_type=" + response_type +
             "&scope=" + scope;
--- GameServer/Services/OAuthService.cs
+++ GameServer/Services/OAuthService.cs
@@ -1,14 +1,27 @@
 ﻿using System.Text.Json;
+using dotenv.net;
 
 namespace GameServer.Services
 {
     public class OAuthService
     {
         private readonly HttpClient _httpClient;
+        private readonly string _googleClientId;
+        private readonly string _googleClientSecret;
 
         public OAuthService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+
+
+            // 환경 변수에서 Client ID, Secret 읽기
+            _googleClientId = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID");
+            _googleClientSecret = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_SECRET");
+
+            if (string.IsNullOrEmpty(_googleClientId) || string.IsNullOrEmpty(_googleClientSecret))
+            {
+                throw new Exception("Google OAuth 환경 변수가 설정되지 않았습니다.");
+            }
         }
 
         public async Task<string> RequestAccessTokenFromKakao(string authorization_code)
@@ -44,7 +57,41 @@ namespace GameServer.Services
             throw new Exception("Access token not found in response");
         }
 
-        public async Task<long> RequestUserIdFromKakao(string access_token)
+        public async Task<string> RequestAccessTokenFromGoogle(string authorization_code)
+        {
+            const string grant_type = "authorization_code";
+
+            const string redirect_uri = "https://localhost:7032/oauth/google";
+
+            string url = "https://oauth2.googleapis.com/token";
+
+            // key-value 형식의 데이터
+            var payload = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("grant_type", grant_type),
+                new KeyValuePair<string, string>("client_id", _googleClientId),
+                new KeyValuePair<string, string>("client_secret", _googleClientSecret),
+                new KeyValuePair<string, string>("redirect_uri", redirect_uri),
+                new KeyValuePair<string, string>("code", authorization_code)
+            };
+
+            var content = new FormUrlEncodedContent(payload);
+
+            var response = await _httpClient.PostAsync(url, content);
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseJson = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(responseString);
+
+            if (responseJson != null && responseJson.TryGetValue("access_token", out var accessTokenElement))
+            {
+                return accessTokenElement.GetString()!;
+            }
+
+            throw new Exception("Access token not found in response");
+        }
+        public async Task<string> RequestUserIdFromKakao(string access_token)
         {
             string url = "https://kapi.kakao.com/v2/user/me";
 
@@ -52,7 +99,7 @@ namespace GameServer.Services
                 new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", access_token);
 
             var response = await _httpClient.GetAsync(url);
-            
+
             response.EnsureSuccessStatusCode();
 
             var responseString = await response.Content.ReadAsStringAsync();
@@ -60,7 +107,29 @@ namespace GameServer.Services
 
             if (responseJson != null && responseJson.TryGetValue("id", out var idElement))
             {
-                return idElement.GetInt64();
+                return idElement.GetInt64().ToString();
+            }
+
+            throw new Exception("Id not found in response");
+        }
+
+        public async Task<string> RequestUserIdFromGoogle(string access_token)
+        {
+            string url = "https://www.googleapis.com/userinfo/v2/me";
+
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", access_token);
+
+            var response = await _httpClient.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseJson = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(responseString);
+
+            if (responseJson != null && responseJson.TryGetValue("id", out var idElement))
+            {
+                return idElement.GetString(); // 사용자 ID를 string으로 반환
             }
 
             throw new Exception("Id not found in response");
